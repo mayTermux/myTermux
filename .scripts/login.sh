70613d60d6f824a7e9a90f9bedd61dc457a850a9
xshin403 <xshin3373@gmail.com>
2021-04-01 11:54:00 +0700

    myTermux-v.0.4.2 pre

--- .scripts/login.sh
+++ .scripts/login.sh
@@ -43,6 +43,9 @@ while true; do
             echo -e "status   :$GREEN Success Login.$DEFAULT"
             sleep 2s
             clear
+            if [ -d ~/.scripts/pid ]; then
+                mkdir ~/.scripts/pid
+            fi
             echo $$ >> ~/.scripts/pid/login
             break
 
