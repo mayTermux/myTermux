e5ba7a6f2148903540be46e553dc5eac44aac063
xshin404 <xshin3373@gmail.com>
2021-08-11 23:12:04 +0700

    myTermux v.0.5.0 dev | termux-login

--- .scripts/login.sh
+++ .scripts/login.sh
@@ -57,6 +57,7 @@ while true; do
       else
         echo -e "\n\nStatus: ${RED}Wrong Password. ${DEFAULT}"
         sleep 2s
+        clear
       fi
 
       # Hidden Password
@@ -91,7 +92,7 @@ while true; do
     else
 
         echo -e ""
-        echo -e "status   :$YELLOW Wrong Username.$DEFAULT"
+        echo -e "Status: $YELLOW Wrong Username.$DEFAULT"
         sleep 2s
         clear
 
