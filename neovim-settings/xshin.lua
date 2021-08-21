eaedd1cc17b87f10684fec5749132becea513047
xshin404 <xshin3373@gmail.com>
2021-08-22 00:33:55 +0700

    Add Cursor Underline when open Neovim

--- neovim-settings/xshin.lua
+++ neovim-settings/xshin.lua
@@ -5,9 +5,6 @@ local cmd = vim.cmd
 -- Return to last edit line position when opening files
 cmd [[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]];
 
--- Set underline cursor
-cmd [[set cursorline]];
-
 -- Set nowrap text when opening files
 cmd [[set nowrap]];
 
