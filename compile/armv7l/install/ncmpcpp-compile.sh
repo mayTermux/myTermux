3cb33e9b03db185d9701993d1898fcb224ee23be
xshin404 <xshin3373@gmail.com>
2021-08-28 00:41:38 +0700

    Ncmpcpp

--- compile/armv7l/install/ncmpcpp-compile.sh
+++ compile/armv7l/install/ncmpcpp-compile.sh
@@ -8,15 +8,15 @@ for DEPENDENCY_PACKAGE in ${DEPENDENCY_PACKAGES[@]}; do
   pkg i -y $DEPENDENCY_PACKAGE
 done
 
-git clone https://github.com/ncmpcpp/ncmpcpp $HOME
+git clone https://github.com/ncmpcpp/ncmpcpp $HOME/ncmpcpp
 cd $HOME/ncmpcpp
 libtoolize && aclocal && autoheader && autoconf && automake --add-missing
 autoupdate
 ./autogen.sh
 sed -i '36i\#include <linux/in.h>' src/screens/visualizer.cpp 
-./configure --enable-outputs --enable-visualizer --enable-clock --enable-static-boost
+./configure --enable-outputs --enable-visualizer --enable-clock --enable-static-boost --with-taglib
 make
 strip src/ncmpcpp
-cp $HOME/myTermux/compile/armv7l/config/ncmpcpp/config $HOME/.config/ncmpcpp/
+cp $HOME/myTermux/compile/armv7l/config/ncmpcpp/config $PREFIX/bin/ncmpcpp
 mpd
 ncmpcpp
