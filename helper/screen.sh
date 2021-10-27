3c1008dc0365f359bd051502e614fb207b57f454
xshin404 <xshin3373@gmail.com>
2021-10-27 22:42:02 +0700

    Script Check Screen Size

--- helper/screen.sh
+++ helper/screen.sh
@@ -13,6 +13,16 @@ function screenSize() {
     
     else
 
+      echo -e "[ ${COLOR_WARNING}INFO${COLOR_BASED} ]  > Your Screen Size
+            Column: '${COLOR_WARNING}${COLS}${COLOR_BASED}'
+            Lines : '${COLOR_WARNING}${ROWS}${COLOR_BASED}'
+            "
+
+      echo -e "[ ${COLOR_SUCCESS}INFO${COLOR_BASED} ]  > Require Screen Size
+            Column: '${COLOR_SUCCESS}101${COLOR_BASED}'
+            Lines : '${COLOR_SUCCESS}39${COLOR_BASED}'
+            "
+
       echo -e "[ ${COLOR_WARNING}ERROR${COLOR_BASED} ] > Please ${COLOR_WARNING}zoom out${COLOR_BASED} your Terminal Screen!"
 
     fi
