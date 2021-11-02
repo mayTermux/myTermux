32803388fdc2eadd4a391be305fc17556566a2d9
xshin404 <xshin3373@gmail.com>
2021-11-02 21:22:33 +0700

    Termux:API Alert

--- helper/finish.sh
+++ helper/finish.sh
@@ -5,3 +5,27 @@ function alertFinish() {
   echo -e "‏‏‎‏‏‎\n    ‎‏‏‎⚠️ Installation Finish, but you need restart Termux to clear setup\n"
 
 }
+
+function alertNotification() {
+
+  IMAGE_PATH="${HOME}/.config/mytermux/alert/images"
+  IMAGE_FILE_NAME="finish.png"
+
+  termux-notification --sound -t "myTermux ${VERSION} has been installed" --image-path ${IMAGE_PATH}/${IMAGE_FILE_NAME}
+
+}
+
+function alertTorch() {
+
+  termux-toast -b "#A8D7FE" -c "#373E4D" -g middle "myTermux ${VERSION} has been installed"
+
+}
+
+
+function mainAlert() {
+
+  alertFinish
+  alertNotification
+  alertTorch
+
+}
--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ function main() {
   NvChad
   utility
 
-  alertFinish
+  mainAlert
 
 }
 
